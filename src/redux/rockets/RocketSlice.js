4d7e0d0bd545407c2f6005bd290914864ce19f48
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-30 01:17:16 +0300

    Update RocketSlice.js

--- src/redux/rockets/RocketSlice.js
+++ src/redux/rockets/RocketSlice.js
@@ -2,11 +2,17 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const rocketSlice = createSlice({
   name: 'rocket',
-  initialState: [], // Initial state should be an empty array
+  initialState: [],
   reducers: {
     setRockets: (state, action) => action.payload,
+    reserveRocket: (state, action) => {
+      const rocketId = action.payload;
+      const newState = state.map((rocket) => (rocket.id !== rocketId ? rocket : { ...rocket, reserved: true }));
+      console.log('New state after reserving rocket:', newState);
+      return newState;
+    },
   },
 });
 
-export const { setRockets } = rocketSlice.actions;
+export const { setRockets, reserveRocket } = rocketSlice.actions;
 export default rocketSlice.reducer;
