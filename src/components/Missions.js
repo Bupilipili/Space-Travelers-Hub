fd96d80ade48ba8ea6f9706d5b19d4e2cb97f027
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-30 01:46:11 +0300

    Update Missions.js

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,29 +1,9 @@
-
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  setMissions,
-} from '../redux/missions/MissionSlice';
-import './styles/Missions.css';
-
 function Missions() {
-  const dispatch = useDispatch();
-  const missionItem = useSelector((state) => state.missions);
-  useEffect(() => {
-    const fetchMissions = async () => {
-      try {
-        const response = await fetch('https://api.spacexdata.com/v3/missions');
-        const data = await response.json();
-        dispatch(setMissions(data));
-      } catch (error) {
-        console.error('Error fetching missions:', error);
-      }
-    };
-
-    fetchMissions();
-  }, [dispatch]);
   return (
-    { missionItem }
+    <div>
+      Missions
+    </div>
   );
 }
+
 export default Missions;
