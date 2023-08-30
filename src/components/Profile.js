f1cda91aace8be1201363d9aa299c08e542e69df
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-08-30 13:11:01 +0200

    Display joined mission profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,24 @@
+import { useSelector } from 'react-redux';
+import './styles/Profile.css';
+
 function Profile() {
+  const joinedMissions = useSelector((state) => state.missions.joinedMissions);
+
   return (
-    <div>
-      <h2>My Profile</h2>
+    <div className="mission-container">
+      <h2>My Missions</h2>
+      <div className="joinedMission">
+        {joinedMissions.length === 0 && <p className="no-mission">No mission joined</p>}
+        {joinedMissions.length > 0 && (
+          <ul>
+            {joinedMissions.map((missionName) => (
+              <li key={missionName.mission_id}>
+                {missionName.mission_name}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
