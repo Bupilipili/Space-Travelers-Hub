3d0b056818cc8cb8449bce323c337e8e07cae8df
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-30 13:05:36 +0300

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,29 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
 function Profile() {
+  const rockets = useSelector((state) => state.rocket);
+
+  // Filter reserved rockets
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+
   return (
     <div>
       <h2>My Profile</h2>
+      {reservedRockets.length > 0 ? (
+        <div>
+          <h3>Reserved Rockets</h3>
+          <ul>
+            {reservedRockets.map((rocket) => (
+              <li key={rocket.id}>
+                <p>{rocket.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <p>You haven&apos;t reserved any rockets yet.</p>
+      )}
     </div>
   );
 }
