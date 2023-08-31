b7d9e84a0d1eac766bff35501baaab4548f49eb4
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-08-31 09:55:16 +0200

    Missions file changes

--- src/redux/missions/MissionSlice.js
+++ src/redux/missions/MissionSlice.js
@@ -2,12 +2,17 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const missionSlice = createSlice({
   name: 'mission',
-  initialState: { missions: [] },
+  initialState: 
+  { missions: [],
+  joinedMissions: [],
+  loading: false,
+  error: null,
+  },
 
   reducers: {
-    setMissions: (state, action) => {
-      state.missions = action.payload;
-    },
+    // setMissions: (state, action) => {
+    //   state.missions = action.payload;
+    // },
     joinMission: (state, action) => {
       const missionId = action.payload;
       state.missions = state.missions.map((mission) => (mission.mission_id === missionId
@@ -20,6 +25,21 @@ const missionSlice = createSlice({
         ? { ...mission, reserved: false }
         : mission));
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMissions.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchMissions.fulfilled, (state, action) => {
+        state.loading = false;
+        state.missions = action.payload;
+      })
+      .addCase(fetchMissions.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
   },
 });
 
